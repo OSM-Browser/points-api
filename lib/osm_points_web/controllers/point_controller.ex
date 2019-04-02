7022d4e61318fc8da507809302a57bf38223207a
duxet <duxetlg@gmail.com>
2019-04-02 14:07:19 +0200

    Add full text search

--- lib/osm_points_web/controllers/point_controller.ex
+++ lib/osm_points_web/controllers/point_controller.ex
@@ -2,25 +2,36 @@ defmodule OsmPointsWeb.PointController do
   use OsmPointsWeb, :controller
   import Ecto.Query
   import Geo.PostGIS
-  
+
   def index(conn, params) do
     points = OsmPoints.Point
       |> preload(:tags)
       |> filter_by_bbox(params)
+      |> filter_by_query(params)
       |> filter_by_type(params)
       |> filter_by_subtype(params)
+      |> limit(100)
       |> OsmPoints.Repo.all
 
     json conn, points
   end
 
   defp filter_by_bbox(query, params) do
-    [minX, minY, maxX, maxY] = params["bbox"] |> String.split(",")
-    bbox = %Geo.Polygon{ coordinates: [ [
-      {minX, minY}, {minX, maxY}, {maxX, maxY}, {maxX, minY}, {minX, minY}
-    ] ] }
+    params["bbox"]
+    |> case do
+      nil  -> query
+      ""   -> query
+      text -> query |> where([p], st_intersects(p.location, ^to_polygon(text)))
+    end
+  end
 
-    query |> where([p], st_intersects(p.location, ^bbox))
+  defp filter_by_query(query, params) do
+    params["query"]
+    |> case do
+      nil  -> query
+      ""   -> query
+      text -> query |> where([p], fragment("name_tokens @@ to_tsquery(?)", ^text))
+    end
   end
 
   defp filter_by_type(query, params) do
@@ -39,5 +50,13 @@ defmodule OsmPointsWeb.PointController do
       ""   -> query
       text -> query |> where([p], p.subtype == ^text)
     end
+  end
+
+  defp to_polygon(text) do
+    [minX, minY, maxX, maxY] = text |> String.split(",")
+
+    %Geo.Polygon{ coordinates: [ [
+      {minX, minY}, {minX, maxY}, {maxX, maxY}, {maxX, minY}, {minX, minY}
+    ] ] }
   end
 end
